9fd69776f9d5a9875a77148c488efcc593a76d86
Dzmitry <dzmitry.kovalenok@thehamon.com>
2018-09-08 13:54:41 +0300

    Init gem

---
+++ lib/acts/as/permissible.rb
@@ -0,0 +1,9 @@
+require "acts/as/permissible/version"
+
+module Acts
+  module As
+    module Permissible
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/acts/as/permissible/version.rb
@@ -0,0 +1,7 @@
+module Acts
+  module As
+    module Permissible
+      VERSION = "0.1.0"
+    end
+  end
+end
